cbf61d54ac7ddd34b220e6ebf544816b2bfbf69f
KJH <ainklain@gmail.com>
2018-11-07 15:18:36 +0900

    [DeepInv] layers visualization func added and env revised

--- qrl/env_factor.py
+++ qrl/env_factor.py
@@ -17,17 +17,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-def _prices2returns(prices):
-    px = pd.DataFrame(prices)
-    nl = px.shift().fillna(0)
-    R = ((px - nl)/nl).fillna(0).replace([np.inf, -np.inf], np.nan).dropna()
-    R = np.append(R[0].values, 0)
-
-    return R
-
-
 class DataGenerator:
-    def __init__(self, history, stock_list, marketdate, steps=730, window_len=50, start_date=None):
+    def __init__(self, history, asset_list, marketdate, steps=730, window_len=50, start_date=None):
         import copy
 
         self.step = 0
@@ -36,14 +27,14 @@ class DataGenerator:
         self.start_date = start_date
 
         self._data = history.copy()
-        self.asset_names = copy.copy(stock_list)
+        self.asset_names = copy.copy(asset_list)
         self.marketdate = copy.copy(marketdate)
 
     def _step(self):
         self.step += 1
-        obs = self.data[self.step: (self.step + self.window_len), :, :].copy()
+        obs = self.data[self.step: (self.step + self.window_len), :].copy()
 
-        ground_truth_obs = self.data[(self.step + self.window_len): (self.step + self.window_len) + 1, :, :].copy()
+        ground_truth_obs = self.data[(self.step + self.window_len): (self.step + self.window_len) + 1, :].copy()
         done = self.step >= self.steps
         return obs, done, ground_truth_obs
 
@@ -57,11 +48,11 @@ class DataGenerator:
         else:
             self.idx = self.marketdate.index(np.datetime64(self.start_date))
 
-        data = self._data[(self.idx - self.window_len): (self.idx + self.steps + 1), :, :4]
+        data = self._data[(self.idx - self.window_len): (self.idx + self.steps + 1), :]
         self.data = data
 
-        return self.data[self.step: (self.step + self.window_len), :, :].copy(), \
-               self.data[(self.step + self.window_len): (self.step + self.window_len + 1), :, :].copy()
+        return self.data[self.step: (self.step + self.window_len), :].copy(), \
+               self.data[(self.step + self.window_len): (self.step + self.window_len + 1), :].copy()
 
 
 class PortfolioSim(object):
@@ -90,17 +81,16 @@ class PortfolioSim(object):
         self.actions.fill(0)
         self.navs.fill(1)
         self.stk_nav.fill(1)
-        self.strat_returns.fill(1)
+        self.strat_returns.fill(0)
         self.positions.fill(0)
         self.costs.fill(0)
         self.trades.fill(0)
-        self.stk_returns.fill(1)
+        self.stk_returns.fill(0)
 
     def _step(self, actions, stk_returns):
         """ actions: weights
             stk_returns: y of stocks
         """
-        assert stk_returns[0] == 1.0
         eps = 1e-8
 
         if self.step == 0:
@@ -115,17 +105,17 @@ class PortfolioSim(object):
         self.stk_returns[self.step, :] = stk_returns
         self.actions[self.step, :] = actions
 
-        self.positions[self.step, :] = (stk_returns * actions) / (np.dot(stk_returns, actions) + eps)
+        self.positions[self.step, :] = ((stk_returns + 1.) * actions) / (np.dot((stk_returns + 1.), actions) + eps)
         self.trades[self.step, :] = actions - last_pos
 
         trade_costs_pct = np.sum(abs(self.trades[self.step, :])) * self.trading_cost
         self.costs[self.step] = trade_costs_pct + self.time_cost
-        reward = np.dot(stk_returns, actions) - self.costs[self.step]
+        reward = (np.dot((stk_returns + 1.), actions) - 1.) - self.costs[self.step]
         self.strat_returns[self.step] = reward
 
         if self.step != 0:
-            self.navs[self.step] = last_nav * self.strat_returns[self.step]
-            self.stk_nav[self.step, :] = stk_nav * self.stk_returns[self.step, :]
+            self.navs[self.step] = last_nav * (1. + self.strat_returns[self.step])
+            self.stk_nav[self.step, :] = stk_nav * (1. + self.stk_returns[self.step, :])
 
         info = {'reward': reward, 'nav': self.navs[self.step], 'costs': self.costs[self.step]}
 
@@ -138,7 +128,6 @@ class PortfolioSim(object):
         """returns internal state in new dataframe """
         cols = ['action', 'bod_nav', 'mkt_nav', 'mkt_return', 'sim_return',
                 'position', 'costs', 'trade']
-        rets = _prices2returns(self.navs)
         # pdb.set_trace()
         df = pd.DataFrame({'action': self.actions,  # today's action (from agent)
                            'bod_nav': self.navs,  # BOD Net Asset Value (NAV)
@@ -155,65 +144,63 @@ class PortfolioSim(object):
 class PortfolioEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, history, stock_list, marketdate, steps=730, trading_cost=0.0025, time_cost=0.00,
-                 window_len=50, sample_start_date=None):
+    def __init__(self, history, asset_list, marketdate, steps=730, trading_cost=0.0,  # 0.0025
+                 time_cost=0.00,
+                 window_length=50, sample_start_date=None):
         assert history.shape[0] == len(marketdate)
-        assert history.shape[1] == len(stock_list)
+        assert history.shape[1] == len(asset_list)
 
         super().__init__()
-        self.window_len = window_len
-        self.num_stocks = len(stock_list)
+        self.window_length = window_length
+        self.num_assets = len(asset_list)
+        self.asset_list = asset_list
         self.steps = steps
 
-        self.src = DataGenerator(history, stock_list, marketdate,
+        self.src = DataGenerator(history, asset_list, marketdate,
                                  steps=steps,
-                                 window_len=window_len,
+                                 window_len=window_length,
                                  start_date=sample_start_date)
 
-        asset_names = ['cash'] + stock_list
-        self.sim = PortfolioSim(asset_names=asset_names,
+        self.sim = PortfolioSim(asset_names=asset_list,
                                 steps=steps,
                                 trading_cost=trading_cost,
                                 time_cost=time_cost)
 
-        self.action_space = gym.spaces.Box(0, 1, shape=(self.num_stocks + 1, ), dtype=np.float32)
+        self.action_space = gym.spaces.Box(0, 1, shape=(self.num_assets, ), dtype=np.float32)
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf,
-                                        shape=(window_len, self.num_stocks, history.shape[-1]),
+                                        shape=(window_length, self.num_assets),
                                         dtype=np.float32)
 
     def step(self, action):
         return self._step(action)
 
     def _step(self, action, eps=1e-8):
-        np.testing.assert_almost_equal(action.shape, (self.num_stocks + 1,))
+        np.testing.assert_almost_equal(action.shape, (self.num_assets,))
+        # action = np.clip(action, 0, 1)
+        #
+        # weights = action
+        # weights /= (weights.sum() + eps)
+        # weights[0] += np.clip(1 - weights.sum(), 0, 1)
         action = np.clip(action, 0, 1)
+        weights = np.zeros_like(action)
+        weights[action > 0] = action[action > 0] / (np.sum(action[action > 0]) + eps)
+        # weights[action < 0] = action[action < 0] / (np.sum(action[action < 0]) + eps)
 
-        weights = action
-        weights /= (weights.sum() + eps)
-        weights[0] += np.clip(1 - weights.sum(), 0, 1)
 
-        assert ((weights >= 0) * (weights <= 1)).all()
+        assert (weights >= (-1 - 1e-6) * (weights <= (1 + 1e-6))).all()
         np.testing.assert_almost_equal(np.sum(weights), 1.0, 3)
+        # np.testing.assert_almost_equal(np.sum(weights), 0.0, 3)
 
         obs, done1, ground_truth_obs = self.src._step()
 
-        cash_obs = np.ones((self.window_len, 1, obs.shape[2]))
-        obs = np.concatenate((cash_obs, obs), axis=1)
-
-        cash_ground_truth = np.ones((1, 1, ground_truth_obs.shape[2]))
-        ground_truth_obs = np.concatenate((cash_ground_truth, ground_truth_obs), axis=1)
-
-        close_p_vector = obs[-1, :, 3]
-        open_p_vector = obs[-1, :, 0]
-        y1 = close_p_vector / open_p_vector
+        y1 = obs[-1, :]
         reward, info, done2 = self.sim._step(weights, y1)
 
         self.infos.append(info)
-        obs = obs[:, :, 3:4] / obs[:, :, 0:1]
 
+        obs = np.expand_dims(obs, -1)
         # print('reward:{} , info:{}'.format(reward, info))
         return obs, reward, done1 or done2, info
-
 
     def reset(self):
         return self._reset()
@@ -222,19 +209,10 @@ class PortfolioEnv(gym.Env):
         self.infos = []
         self.sim.reset()
         obs, ground_truth_obs = self.src.reset()
-        cash_obs = np.ones((self.window_len, 1, obs.shape[2]))
-        obs = np.concatenate((cash_obs, obs), axis=1)
-        # cash_ground_truth = np.ones((1, 1, ground_truth_obs.shape[2]))
-        # ground_truth_obs = np.concatenate((cash_ground_truth, ground_truth_obs), axis=1)
+        obs = np.expand_dims(obs, -1)
 
         self.render_call = 0
-        # ax2 = self.fig.add_subplot(21)
 
-        # ax = self.fig.add_subplot(111)
-        # self.line, = ax.plot([], [], 'k-')
-
-
-        obs = obs[:, :, 3:4] / obs[:, :, 0:1]
         return obs
         # return obs, info
 
@@ -247,78 +225,30 @@ class PortfolioEnv(gym.Env):
         if mode == 'ansi':
             pprint(self.infos[-1])
         elif mode == 'human':
-            # if self.fig is None:
-            #     self.fig = plt.figure()
-            #
-            #     ax = self.fig.add_subplot(111)
-            #     self.line, = ax.plot([], [])
-
-            # self.plot()
             if self.render_call == 0:
                 self.fig = plt.figure()
                 self.ax1, self.ax2, self.ax3 = self.fig.subplots(3, 1)
-                self.render_call += 1
 
             self._get_image()
+
+            if self.render_call == 0:
+                self.ax3.legend(loc='lower center', bbox_to_anchor=(0.5, -0.05), ncol=3, fancybox=True, shadow=True)
+                self.render_call += 1
+
             # self.fig.canvas.draw()
             if self.sim.step % 20 == 0:
                 self.fig.canvas.draw()
                 self.fig.canvas.flush_events()
 
-    def plot(self):
-        x = np.arange(self.steps)
-        y = np.zeros_like(x)
-
-        for i, info in enumerate(self.infos):
-            y[i] = info['nav']
-        self.line.set_data(x, y)
-        return self.line,
-
-
-
     def _get_image(self):
-        # return self.infos[-1]['market_value']
-
-        import io
-        from PIL import Image
-        # from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-        # from matplotlib.figure import Figure
-        # plt.figure()
-        # plt.plot([1, 2])
-        # plt.title("test")
-        # buf = io.BytesIO()
-        # plt.savefig(buf, format='png')
-        # buf.seek(0)
-        # im = Image.open(buf)
-        # im.show()
-        # buf.close()
-
-        # plt.figure()
-        # df_info = pd.DataFrame(self.infos)
-        # plt.plot(df_info.index, df_info.nav)
-        # buf = io.BytesIO()
-        # plt.savefig(buf, format='png')
-        # buf.seek(0)
-        # im = Image.open(buf)
-        # buf.close()
-        #
-        # fig = Figure()
-        # canvas = FigureCanvas(fig)
-        # canvas = self.fig.canvas
-        # ax = self.fig.gca()
-        # ax1, ax2 = self.fig.subplots(2, 1)
         df_info = pd.DataFrame(self.infos)
         self.ax1.plot(df_info.index, df_info.nav, color='k')
 
         actions_t = np.transpose(self.sim.actions)
-        pal = sns.color_palette("hls", 19)
+        pal = sns.color_palette("hls", self.num_assets)
         self.ax2.stackplot(df_info.index, actions_t[:, :len(df_info.index)], colors=pal)
 
         stk_nav_t = np.transpose(self.sim.stk_nav)
-        for i in range(self.num_stocks + 1):
-            self.ax3.plot(df_info.index, stk_nav_t[i, :len(df_info.index)], color=pal[i])
+        for i in range(self.num_assets):
+            self.ax3.plot(df_info.index, stk_nav_t[i, :len(df_info.index)], color=pal[i], label=self.asset_list[i])
 
-        # image = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
-        # w, h = canvas.get_width_height()
-        # image.shape = (w, h, 3)
-        # return image
--- qrl/rl_test.py
+++ qrl/rl_test.py
@@ -6,14 +6,17 @@ from gym import spaces
 
 import keras
 from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Flatten, Input, Concatenate, Conv2D, BatchNormalization, Reshape
+from keras.layers import Dense, Activation, Flatten, Input, Concatenate, Conv2D, BatchNormalization, Reshape, LeakyReLU
 from keras.optimizers import Adam
+from keras import backend as K
 
+from qtest.nalu import NALU
 from rl.agents import DDPGAgent
 from rl.memory import SequentialMemory
 from rl.random import OrnsteinUhlenbeckProcess
 
-from qrl.env_portfolio2 import PortfolioEnv
+# from qrl.env_portfolio2 import PortfolioEnv
+from qrl.env_factor import PortfolioEnv
 
 
 def read_stock_history():
@@ -50,6 +53,43 @@ def read_stock_history():
     return history, infocode, marketdate, item_list
 
 
+def factor_history():
+    from qdata.dbmanager import SqlManager
+    sql_ = """
+        select * 
+            from  (
+            select idxcd, d.eval_d, round(clsprc / lag(clsprc, 1) over (partition by idxcd order by base_d) - 1, 5) as value_
+                from (
+                    select eval_d
+                    from qinv..DateTableGlobal
+                    where region = 'KR'
+                    and eval_d = work_d
+                    and eval_d >= '2005-02-01'
+                ) D
+                join wms..indexdata a
+                on d.eval_d = convert(date, a.base_d)
+                where idxcd in ('MOM','BEME','GPA','USDKRW','KISCOMPBONDCALL')
+                and base_d >= '20050201'
+            ) A
+            pivot (
+                min(value_)
+                for idxcd in ([MOM],[BEME],[GPA],[USDKRW],[KISCOMPBONDCALL])
+            ) B
+            order by eval_d
+    """
+    sqlm = SqlManager()
+    df = sqlm.db_read(sql_)
+    df.columns = [i.lower() for i in df.columns]
+    df.set_index('eval_d', inplace=True)
+    df = df[df.isna().sum(axis=1) == 0]
+    factor_id = list(df.columns)
+    marketdate = list(df.index.unique())
+
+    history = df.values
+
+    # history = df_to_arr[~np.isnan(np.sum(df_to_arr, axis=1)), :]
+
+    return history, factor_id, marketdate
 
 
 
@@ -61,24 +101,24 @@ def main():
     window_length = 50
     nb_actions = len(target_stocks) + 1
 
-    action_dim = [nb_actions]
-    state_dim = [window_length, nb_actions]
-    batch_size = 64
-    action_bound = 1.
-    tau = 1e-3
-    learning_rate = 1e-4
-
-    predictor_type = 'cnn'
-    use_batch_norm = True
-
-    CONFIG = {'seed': 1234,
-              'episode': 1,
-              'batch_size': 256,
-              'gamma': 0.99,
-              'buffer_size': 100,
-              'max_step': 500,
-              'tau': 0.001
-              }
+    # action_dim = [nb_actions]
+    # state_dim = [window_length, nb_actions]
+    # batch_size = 64
+    # action_bound = 1.
+    # tau = 1e-3
+    # learning_rate = 1e-4
+    #
+    # predictor_type = 'cnn'
+    # use_batch_norm = True
+    #
+    # CONFIG = {'seed': 1234,
+    #           'episode': 1,
+    #           'batch_size': 256,
+    #           'gamma': 0.99,
+    #           'buffer_size': 100,
+    #           'max_step': 500,
+    #           'tau': 0.001
+    #           }
 
     # get target history
     target_history = np.empty(shape=(num_training_time, len(target_stocks), history.shape[2]))
@@ -109,6 +149,7 @@ def main():
     x = Dense(64, activation='relu')(x)
     w_init = keras.initializers.RandomUniform(minval=-0.003, maxval=0.003, seed=None)
     x = Dense(nb_actions, activation='softmax', kernel_initializer=w_init)(x)
+    # x = NALU(nb_actions)(x)
     actor = Model(inputs=observation_input, outputs=x)
 
     # actor = Sequential()
@@ -127,18 +168,18 @@ def main():
 
     x = Conv2D(32, kernel_size=(3, 1))(reshaped_obs_input)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(32, kernel_size=(1, 1))(x)
+    l1 = Activation('relu')(x)
+    x = Conv2D(32, kernel_size=(1, 1))(l1)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    flattened_observation = Flatten()(x)
+    l2 = Activation('relu')(x)
+    flattened_observation = Flatten()(l2)
     x = Concatenate()([action_input, flattened_observation])
     x = Dense(64)(x)
-    x = Activation('relu')(x)
-    x = Dense(64)(x)
-    x = Activation('relu')(x)
+    l3 = Activation('relu')(x)
+    x = Dense(64)(l3)
+    l4 = Activation('relu')(x)
     w_init = keras.initializers.RandomUniform(minval=-0.003, maxval=0.003, seed=None)
-    x = Dense(1, activation='linear', kernel_initializer=w_init)(x)
+    x = Dense(1, activation='linear', kernel_initializer=w_init)(l4)
     critic = Model(inputs=[action_input, observation_input], outputs=x)
     print(critic.summary())
 
@@ -154,15 +195,200 @@ def main():
     # Okay, now it's time to learn something! We visualize the training here for show, but this
     # slows down training quite a lot. You can always safely abort the training prematurely using
     # Ctrl + C.
-    agent.fit(env, nb_steps=500, visualize=True, verbose=1, nb_max_episode_steps=252)
+    agent_history = agent.fit(env, nb_steps=1000000, visualize=False, verbose=1, nb_max_episode_steps=252)
+
 
 
     # After training is done, we save the final weights.
-    # agent.save_weights('ddpg_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+    agent.save_weights('ddpg_{}_weights.h5f'.format('RL_ENV2_TEST'), overwrite=True)
 
     # Finally, evaluate our algorithm for 5 episodes.
-    # agent.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=200)
+    agent.test(env, nb_episodes=5, visualize=False, nb_max_episode_steps=252)
+
+
+def main2():
+
+    history, factor_id, marketdate = factor_history()
+    target_assets = factor_id
+    window_length = 50
+    mem_size = 100
+    steps = 252
+    nb_actions = len(target_assets)
+
+
+    # get target history
+    import copy
+    target_history = copy.deepcopy(history)
+    target_marketdate = copy.deepcopy(marketdate)
+
+    env = PortfolioEnv(target_history, target_assets, target_marketdate, steps=steps, window_length=window_length,
+                       trading_cost=0.00)
+
+    action_input = Input(shape=(nb_actions,), name='action_input')
+    observation_input = Input(shape=(1, window_length, nb_actions, 1), name='observation_input')
+    reshaped_obs_input = Reshape((window_length, nb_actions, 1))(observation_input)
+    x = Conv2D(10, kernel_size=(30, 1))(reshaped_obs_input)
+    x = BatchNormalization()(x)
+    x = Activation('linear', name='actor_layer_1')(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(10, kernel_size=(1, 1))(x)
+    x = BatchNormalization()(x)
+    x = Activation('linear', name='actor_layer_2')(x)
+    x = LeakyReLU()(x)
+    flattened_observation = Flatten()(x)
+    x = Dense(64, activation='linear')(flattened_observation)
+    x = LeakyReLU()(x)
+    x = Dense(32, activation='linear')(x)
+    x = LeakyReLU(name='actor_layer_3')(x)
+    w_init = keras.initializers.RandomUniform(minval=-0.003, maxval=0.003, seed=None)
+    x = Dense(nb_actions, activation='softmax', kernel_initializer=w_init)(x)
+    actor = Model(inputs=observation_input, outputs=x)
+    actor_intermediate_1 = Model(inputs=actor.inputs, outputs=actor.get_layer('actor_layer_1').output)
+    actor_intermediate_2 = Model(inputs=actor.inputs, outputs=actor.get_layer('actor_layer_2').output)
+    actor_intermediate_3 = Model(inputs=actor.inputs, outputs=actor.get_layer('actor_layer_3').output)
+    print(actor.summary())
+
+    # x = Conv2D(32, kernel_size=(50, 1))(reshaped_obs_input)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(32, kernel_size=(1, 1))(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # flattened_observation = Flatten()(x)
+    x = Concatenate()([action_input, flattened_observation])
+    x = Dense(64, activation='linear', name='critic_layer_1')(x)
+    x = LeakyReLU()(x)
+    x = Dense(32, activation='linear', name='critic_layer_2')(x)
+    x = LeakyReLU()(x)
+    w_init = keras.initializers.RandomUniform(minval=-0.003, maxval=0.003, seed=None)
+    x = Dense(1, activation='linear', kernel_initializer=w_init, name='critic_layer_3')(x)
+    critic = Model(inputs=[action_input, observation_input], outputs=x)
+    critic_intermediate_1 = Model(inputs=critic.inputs, outputs=critic.get_layer('critic_layer_1').output)
+    critic_intermediate_2 = Model(inputs=critic.inputs, outputs=critic.get_layer('critic_layer_2').output)
+    critic_intermediate_3 = Model(inputs=critic.inputs, outputs=critic.get_layer('critic_layer_3').output)
+    print(critic.summary())
+
+    # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+    # even the metrics!
+    memory = SequentialMemory(limit=100000, window_length=1)
+    random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.3)
+    agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
+                      memory=memory, nb_steps_warmup_critic=mem_size, nb_steps_warmup_actor=mem_size,
+                      random_process=random_process, gamma=.90, target_model_update=1e-3)
+    agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+
+    # Okay, now it's time to learn something! We visualize the training here for show, but this
+    # slows down training quite a lot. You can always safely abort the training prematurely using
+    # Ctrl + C.
+
+    n_loop = 10
+    output_actor_1 = np.zeros([n_loop, actor_intermediate_1.output_shape[1], actor_intermediate_1.output_shape[2]])
+    output_actor_2 = np.zeros([n_loop, actor_intermediate_2.output_shape[1], actor_intermediate_2.output_shape[2]])
+    output_actor_3 = np.zeros([n_loop, actor_intermediate_3.output_shape[1]])
+    output_critic_1 = np.zeros([n_loop, critic_intermediate_1.output_shape[1]])
+    output_critic_2 = np.zeros([n_loop, critic_intermediate_2.output_shape[1]])
+    output_critic_3 = np.zeros([n_loop, critic_intermediate_3.output_shape[1]])
+
+    for l in range(n_loop):
+        agent_history = agent.fit(env, nb_steps=steps*1, visualize=False, verbose=1, nb_max_episode_steps=steps)
+        # obs, _, _ = env.src._step()
+        recent_obs = agent.recent_observation.reshape(1, 1, 50, 5, 1)
+        recent_action = agent.recent_action.reshape(1, 5)
+        output_actor_1[l] = actor_intermediate_1.predict(recent_obs)[0, :, :, 0]
+        output_actor_2[l] = actor_intermediate_2.predict(recent_obs)[0, :, :, 0]
+        output_actor_3[l] = actor_intermediate_3.predict(recent_obs)
+        # print("1: {}, 2:{}, 3:{}, 0's:{}, 1's:{}".format(np.mean(output_actor_1.squeeze(), axis=2),
+        #                                                  np.mean(output_actor_2.squeeze(), axis=2),
+        #                                                  output_actor_3,
+        #                                                  np.sum(output_actor_3 == 0),
+        #                                                  np.sum(output_actor_3 > 0)))
+
+        output_critic_1[l] = critic_intermediate_1.predict([recent_action, recent_obs])
+        output_critic_2[l] = critic_intermediate_2.predict([recent_action, recent_obs])
+        output_critic_3[l] = critic_intermediate_3.predict([recent_action, recent_obs])
+        print("1: {}, 2:{}, 3:{}".format(output_critic_1[l], output_critic_2[l], output_critic_3[l]))
+
+        # agent.test(env, nb_episodes=1, visualize=True, nb_max_episode_steps=steps)
+    plot_layer_3d(output_actor_1)
+    plot_layer_3d(output_actor_2)
+    plot_layer_2d(output_actor_3)
+    plot_layer_2d(output_critic_1)
+    plot_layer_2d(output_critic_2)
+    plot_layer_2d(output_critic_3)
+
+    # test
+    asset_list = target_assets
+    trading_cost = 0.0
+    time_cost = 0.00
+    window_length = 50
+    sample_start_date = None
+    num_assets = len(asset_list)
+    src = DataGenerator(history, asset_list, marketdate,
+                        steps=steps,
+                        window_len=window_length,
+                        start_date=sample_start_date)
+    sim = PortfolioSim(asset_names=asset_list,
+                       steps=steps,
+                       trading_cost=trading_cost,
+                       time_cost=time_cost)
+
+
+def plot_layer_2d(mat):
+
+    import matplotlib.pyplot as plt
+    from matplotlib.pyplot import cm
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    x, y = np.meshgrid(range(mat.shape[1]), range(mat.shape[0]))
+    xs = x.reshape(-1)
+    ys = y.reshape(-1)
+    cols = mat.reshape(-1) - np.min(mat)
+    colors = cm.hsv(cols / max(cols))
+
+
+    colormap = cm.ScalarMappable(cmap=cm.hsv)
+    colormap.set_array(cols)
+    ax.scatter(xs, ys, c=colors)
+    cb = fig.colorbar(colormap)
+    plt.title("subtracted:{}".format(np.min(mat)))
+    plt.show()
+
+
+
+def plot_layer_3d(mat):
+
+    import matplotlib.pyplot as plt
+    from matplotlib.pyplot import cm
+    from mpl_toolkits.mplot3d import Axes3D
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x, y, z = np.meshgrid(range(mat.shape[1]),
+                          range(mat.shape[0]),
+                          range(mat.shape[2]))
+    xs = x.reshape(-1)
+    ys = y.reshape(-1)
+    zs = z.reshape(-1)
+    cols = mat.reshape(-1) - np.min(mat)
+    colors = cm.hsv(cols / max(cols))
+
+
+    colormap = cm.ScalarMappable(cmap=cm.hsv)
+    colormap.set_array(cols)
+    ax.scatter(xs, ys, zs, c=colors)
+    cb = fig.colorbar(colormap)
+    plt.title("subtracted:{}".format(np.min(mat)))
+    plt.show()
+
 
 
 if __name__ == '__main__':
-    main()
+    main2()
+
+
+#
+# https://www.alexirpan.com/2018/02/14/rl-hard.html
+# https://himanshusahni.github.io/2018/02/23/reinforcement-learning-never-worked.html
